9b262dde3652afae62f7e838a085b0dd55df0d29
yu-2022 <109321653+yu-2022@users.noreply.github.com>
2022-08-19 14:10:31 +0900

    ユーザー編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   root to: "lists#index"
   resources :users, only: [:index, :show]
 end
